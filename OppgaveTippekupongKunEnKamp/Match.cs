26b5337e7a6e775ff1af3d5c593592744e5b1487
martinlrmr <102468994+MartinL-no@users.noreply.github.com>
2023-03-26 19:20:52 +0200

    my solution

---
+++ OppgaveTippekupongKunEnKamp/Match.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace OppgaveTippekupongKunEnKamp
+{
+    public class Match
+    {
+        public int HomeGoals;
+        public int AwayGoals;
+        public string Result => HomeGoals == AwayGoals ? "U" : HomeGoals > AwayGoals ? "H" : "B";
+
+        public Match()
+        {
+            HomeGoals = 0;
+            AwayGoals = 0;
+        }
+        public bool IsBetCorrect(string bet)
+        {
+            return bet.Contains(Result);
+        }
+        public string isBetCorrectText(string? bet)
+        {
+            if (bet != null && bet.Contains(Result))
+            {
+                return "correctly";
+            }
+            else
+            {
+                return "falsely";
+            }
+        }
+    }
+}
+
---
+++ OppgaveTippekupongKunEnKamp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Reflection;
+using System.Runtime.Intrinsics.X86;
+namespace OppgaveTippekupongKunEnKamp;
+class Program
+{
+    static void Main(string[] args)
+    {
+        RunGame();
+    }
+
+    private static void RunGame()
+    {
+        Console.Write("Valid tips: \r\n - H, U, B\r\n - half guard: HU, HB, UB\r\n - full guard: HUB\r\nWhat have you tipped for this match? ");
+        var bet = Console.ReadLine();
+        var match = new Match();
+        var matchIsRunning = true;
+
+        while (matchIsRunning)
+        {
+            Console.Write("Commands: \r\n - H = scoring home team\r\n - B = scoring away team\r\n - X = match is over\r\nEnter command: ");
+            var command = Console.ReadLine();
+            matchIsRunning = command != "X";
+            if (command == "H") match.HomeGoals++;
+            else if (command == "B") match.AwayGoals++;
+            Console.WriteLine($"The position is {match.HomeGoals}-{match.AwayGoals}.");
+        }
+
+        var isBetCorrectText = match.isBetCorrectText(bet);
+        Console.WriteLine($"You tipped {isBetCorrectText}");
+
+    }
+}
+
